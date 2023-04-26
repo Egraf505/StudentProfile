0ac633a7cda8c2f958ecd43a750ae700dcea0dde
Egraf505 <egor.leskov.03@mail.ru>
2023-04-26 23:39:21 +0300

    Изменен ответ получение студента Добавлено получение мероприятий по id преподователя Добавлено получение мероприятий по Id студента

---
+++ StudentProfile.Application/Events/Queries/GetEventByStudentId/GetEventByStudentIdCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using StudentProfile.Application.Common.Exeptions;
+using StudentProfile.Application.Interfaces;
+using StudentProfile.Domain;
+
+namespace StudentProfile.Application.Events.Queries.GetEventByStudentId
+{
+    public class GetEventByStudentIdCommandHandler : IRequestHandler<GetEventByStudentIdCommand, List<Event>>
+    {
+        private readonly IStudentProfileContext _context;
+
+        public GetEventByStudentIdCommandHandler(IStudentProfileContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Event>> Handle(GetEventByStudentIdCommand request, CancellationToken cancellationToken)
+        {
+            var student = await _context.Students.FirstOrDefaultAsync(x => x.id == request.studentId, cancellationToken);
+
+            if (student == null)
+            {
+                throw new NotFoundException(nameof(Student), request.studentId);
+            }
+
+            var events = await _context.Events.Where(x => x.Students.Contains(student)).ToListAsync(cancellationToken);
+
+            return events;
+        }
+    }
+}
--- StudentProfile.Application/Student/Queries/GetStudentById/StudentVm.cs
+++ StudentProfile.Application/Student/Queries/GetStudentById/StudentVm.cs
@@ -5,6 +5,7 @@ namespace StudentProfile.Application.Student.Queries.GetStudentById
 {
     public class StudentVm : IMapWith<Domain.Student>
     {
+        public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string SurName { get; set; } = null!;
         public string? MiddleName { get; set; }
@@ -12,6 +13,8 @@ namespace StudentProfile.Application.Student.Queries.GetStudentById
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Domain.Student, StudentVm>()
+                .ForMember(studentDto => studentDto.Id,
+                opt => opt.MapFrom(student => student.Id))
                 .ForMember(studentDto => studentDto.Name,
                 opt => opt.MapFrom(student => student.Name))
                 .ForMember(studentDto => studentDto.SurName,
