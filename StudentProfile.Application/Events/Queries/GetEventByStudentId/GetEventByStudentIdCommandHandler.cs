46a6039d40f091923eb655e98612be83678f8c04
EfgraF505 <egor.leskov.03@mail.ru>
2023-06-04 23:39:04 +0300

    Правка

--- StudentProfile.Application/Events/Queries/GetEventByStudentId/GetEventByStudentIdCommandHandler.cs
+++ StudentProfile.Application/Events/Queries/GetEventByStudentId/GetEventByStudentIdCommandHandler.cs
@@ -24,7 +24,7 @@ namespace StudentProfile.Application.Events.Queries.GetEventByStudentId
                 throw new NotFoundException(nameof(Student), request.studentId);
             }
 
-            var events = await _context.Events.Where(x => x.Students.Contains(student)).Include(t => t.CreatedTeacher).ToListAsync(cancellationToken);
+            var events = await _context.Events.Where(x => x.Students.Contains(student)).Include(t => t.CreatedTeacher.Id).ToListAsync(cancellationToken);
 
             return events;
         }
